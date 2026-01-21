2d41c8fd4740bc5d03e0b5d52083ef33afadf57e
chistym17 <mchisty31@gmail.com>
2026-01-21 09:24:03 +0600

    transactions included in blockchain

--- blockchain.js
+++ blockchain.js
@@ -1,9 +1,12 @@
 const Block = require("./block");
+const Transaction = require("./transaction");
 
 class Blockchain {
     constructor() {
         this.chain = [this.creategenesisblock()];
-        this.difficulty = 3;
+        this.difficulty = 2;
+        this.pendingTransactions = [];
+        this.miningReward = 50;
 
     }
 
@@ -11,7 +14,7 @@ class Blockchain {
         return new Block(
             0,
             new Date.toString(),
-            "Genesis Block",
+            [],
             "0"
 
         );
@@ -31,6 +34,9 @@ class Blockchain {
             const currentBlock = this.chain[i];
             const previousBlock = this.chain[i - 1];
 
+            if (!currentBlock.hasValidTransactions())
+                return false;
+
             if (currentBlock.hash !== currentBlock.calculateHash())
                 return false;
 
@@ -40,6 +46,33 @@ class Blockchain {
             return true;
 
         }
+    }
+
+    addTransaction(transaction) {
+        if (!transaction.fromAddress || !transaction.toAddress)
+            throw new Error("Transaction must include valid addresses")
+
+        if (!transaction.isValid())
+            throw new Error("Transaction not valid")
+
+        this.pendingTransactions.push(transaction);
+
+    }
+
+    minePendingTransactions(minerAddress) {
+        const rewardTx = new Transaction(null, minerAddress, this.miningReward)
+        this.pendingTransactions.push(rewardTx);
+
+        const block = new Block(this.chain.length,
+            new Date().toISOString(),
+            this.pendingTransactions,
+            this.getLatesBlock.hash)
+
+        block.mineblock(this.difficulty);
+        this.chain.push(block);
+
+        this.pendingTransactions = [];
+
     }
 
 
